c5c1af352fb44eaa99a77d6c53726296920c5a8c
Bryan <bryan.boram@gmail.com>
2022-05-24 13:55:12 -0300

    Chuvaounaochuva v1

---
+++ js.js
@@ -0,0 +1,49 @@
+function chuva() {
+
+   let estado = document.querySelector("input#estadotxt").value;
+   let cidade = document.querySelector("input#cidadetxt").value;
+   let datainput = document.querySelector("input#datatxt").value;
+
+
+   data = new Date(datainput);
+   dataFormatada = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+   
+   let resultado = document.querySelector("div#resultado");
+
+
+   if(estado.length <= 2 ){
+     
+        var estadoMaiusculo = estado.toUpperCase()
+        resultado.innerHTML = `Parabéns os indios já estão dançando e providenciando sua chuva para ${dataFormatada} em ${cidade} no ${estadoMaiusculo}.`
+
+   } else{
+       resultado.innerHTML = `Parabéns os indios já estão dançando e providenciando sua chuva para ${dataFormatada} em ${cidade} no ${estado}.`
+   }
+}
+   function naochuva() {
+
+    let estado = document.querySelector("input#estadotxt").value;
+    let cidade = document.querySelector("input#cidadetxt").value;
+    let data = document.querySelector("input#datatxt").value;
+ 
+ 
+ 
+    
+    let resultado = document.querySelector("div#resultado");
+ 
+ 
+    if(estado.length <= 2 ){
+      
+         var estadoMaiusculo = estado.toUpperCase()
+         resultado.innerHTML = `Parabéns os indios já estão dançando e providenciando sua não chuva para ${dataFormatada} em ${cidade} no ${estadoMaiusculo}.`
+ 
+    } else{
+        resultado.innerHTML = `Parabéns os indios já estão dançando e providenciando sua não chuva para ${dataFormatada} em ${cidade} no ${estado}.`
+    }
+
+   
+
+
+   
+
+}
